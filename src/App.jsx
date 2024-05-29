456dd771ec73dbc316e38bf6aaf2a4c58580822a
nithin7316 <nithinkumar6330.nk@gmail.com>
2024-05-29 23:02:04 +0530

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Intro from "./components/Intro/intro";
+import Navbar from "./components/NavBar/navbar";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Intro />
+    </div>
+  );
+}
+
+export default App;
