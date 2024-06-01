6c05326dda748b5a02c73a7525acc504e9aa9224
nithin7316 <nithinkumar6330.nk@gmail.com>
2024-06-01 21:12:17 +0530

    main page

--- src/components/Intro/intro.jsx
+++ src/components/Intro/intro.jsx
@@ -1,8 +1,34 @@
 import React from "react";
 import "./intro.css";
+import bg from "../../assets/image.png";
+import { Link } from "react-scroll";
+import btnImg from "../../assets/hireme.png";
 
 const Intro = () => {
-  return <div></div>;
+  return (
+    <div>
+      <section id="intro">
+        <div className="introContent">
+          <span className="hello">Hello,</span>
+          <span className="introText">
+            I'm <span className="introName">Nithin</span>
+            <br />
+            Website Designer
+          </span>
+          <p className="introPara">
+            I am a skilled web designer with experience in creating visual
+          </p>
+          <Link>
+            <button className="btn">
+              <img src={btnImg} alt="Hire Me" />
+              Hire Me
+            </button>
+          </Link>
+        </div>
+        <img src={bg} alt="Profile" className="bg" />
+      </section>
+    </div>
+  );
 };
 
 export default Intro;
