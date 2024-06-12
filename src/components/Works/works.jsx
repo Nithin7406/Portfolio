0c6bd5ecbaaa09df6fe5ad13f24c7c593d5188cb
nithin7316 <nithinkumar6330.nk@gmail.com>
2024-06-12 23:41:38 +0530

    my portfolio

---
+++ src/components/Works/works.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import "./works.css";
+import Portfolio1 from "../../assets/portfolio-1.png";
+import Portfolio2 from "../../assets/portfolio-2.png";
+import Portfolio3 from "../../assets/portfolio-3.png";
+import Portfolio4 from "../../assets/portfolio-4.png";
+import Portfolio5 from "../../assets/portfolio-5.png";
+import Portfolio6 from "../../assets/portfolio-6.png";
+
+const works = () => {
+  return (
+    <section id="works">
+      <h2 className="workstitle">My Portfolio</h2>
+      <span className="workDesc"></span>
+      <div className="worksImgs">
+        <img src={Portfolio1} alt="" className="works" />
+        <img src={Portfolio2} alt="" className="works" />
+        <img src={Portfolio3} alt="" className="works" />
+        <img src={Portfolio4} alt="" className="works" />
+        <img src={Portfolio5} alt="" className="works" />
+        <img src={Portfolio6} alt="" className="works" />
+      </div>
+    </section>
+  );
+};
+
+export default works;
