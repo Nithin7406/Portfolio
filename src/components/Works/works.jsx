0989450bd951c476afbcffa0b9f8eb87a1c33e4d
nithin7316 <nithinkumar6330.nk@gmail.com>
2024-06-13 18:08:00 +0530

    my portfolio

--- src/components/Works/works.jsx
+++ src/components/Works/works.jsx
@@ -10,16 +10,22 @@ import Portfolio6 from "../../assets/portfolio-6.png";
 const works = () => {
   return (
     <section id="works">
-      <h2 className="workstitle">My Portfolio</h2>
-      <span className="workDesc"></span>
+      <h2 className="worksTitle">My Portfolio</h2>
+      <span className="worksDesc">
+        I take pride in paying attention to the smallest details and making sure
+        that my work is pixel perfect. I am excited to bring my skills and
+        experience to help businesses achieve their goals and create a strong
+        online presence."
+      </span>
       <div className="worksImgs">
-        <img src={Portfolio1} alt="" className="works" />
-        <img src={Portfolio2} alt="" className="works" />
-        <img src={Portfolio3} alt="" className="works" />
-        <img src={Portfolio4} alt="" className="works" />
-        <img src={Portfolio5} alt="" className="works" />
-        <img src={Portfolio6} alt="" className="works" />
+        <img src={Portfolio2} alt="" className="worksImg" />
+        <img src={Portfolio1} alt="" className="worksImg" />
+        <img src={Portfolio3} alt="" className="worksImg" />
+        <img src={Portfolio4} alt="" className="worksImg" />
+        <img src={Portfolio5} alt="" className="worksImg" />
+        <img src={Portfolio6} alt="" className="worksImg" />
       </div>
+      <button className="worksBtn">See more</button>
     </section>
   );
 };
