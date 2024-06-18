fc9cefaf08cbf378393cbfdb3776b30a5e2da8d3
nithin7316 <nithinkumar6330.nk@gmail.com>
2024-06-18 22:06:55 +0530

    final

--- src/components/Contact/contact.jsx
+++ src/components/Contact/contact.jsx
@@ -1,14 +1,57 @@
 import React from "react";
+import "./contact.css";
+import Walmart from "../../assets/walmart.png";
+import Adobe from "../../assets/adobe.png";
+import Microsoft from "../../assets/microsoft.png";
+import Facebook from "../../assets/facebook.png";
+import FacebookIcon from "../../assets/facebook-icon.png";
+import TwitterIcon from "../../assets/twitter.png";
+import YoutubeIcon from "../../assets/youtube.png";
+import InstagramIcon from "../../assets/instagram.png";
 
-const contact = () => {
+const Contact = () => {
   return (
     <section className="contactPage">
-      <div className="clients">
-        <h1 className="contactPageTitle">My Client</h1>
+      <div id="clients">
+        <h1 className="contactPageTitle">My Clients</h1>
+        <p className="clientDesc">
+          I have had the opportunity to work with a diverse group of companies.
+          Some of the notable companies I have worked with include:
+        </p>
+        <div className="clientImgs">
+          <img src={Walmart} alt="Walmart" className="clientImg" />
+          <img src={Adobe} alt="Adobe" className="clientImg" />
+          <img src={Microsoft} alt="Microsoft" className="clientImg" />
+          <img src={Facebook} alt="Facebook" className="clientImg" />
+        </div>
       </div>
-      <div className="contact"></div>
+      <div id="contact">
+        <h1 className="contactPageTitle">Contact Me</h1>
+        <span className="contactDesc">
+          Please fill out the form below to discuss any work opportunities.
+        </span>
+        <form className="contactForm">
+          <input type="text" className="name" placeholder="Your Name" />
+          <input type="email" className="email" placeholder="Your Email" />
+          <textarea
+            className="msg"
+            name="message"
+            rows="5"
+            placeholder="Your Message"
+          ></textarea>
+          <button type="submit" className="submitBtn">
+            Submit
+          </button>
+          <div className="links">
+            <img src={FacebookIcon} alt="Facebook" className="link" />
+            <img src={TwitterIcon} alt="Twitter" className="link" />
+            <img src={YoutubeIcon} alt="YouTube" className="link" />
+            <img src={InstagramIcon} alt="Instagram" className="link" />
+          </div>
+        </form>
+      </div>
     </section>
   );
 };
 
-export default contact;
+export default Contact;
--- src/components/NavBar/navbar.jsx
+++ src/components/NavBar/navbar.jsx
@@ -1,31 +1,140 @@
-import React from "react";
+import React, { useState } from "react";
 import "./navbar.css";
 import logo from "../../assets/logo.png";
 import contactImg from "../../assets/contact.png";
 import { Link } from "react-scroll";
+import menu from "../../assets/menu.png";
 
 const Navbar = () => {
+  const [showMenu, setShowMenu] = useState(false);
   return (
     <nav className="navbar">
       <img src={logo} alt="Logo" className="logo" />
       <div className="desktopMenu">
-        <Link to="/" className="decktopMenuListItem">
+        <Link
+          activeClass="active"
+          to="intro"
+          spy={true}
+          smooth={true}
+          offset={-100}
+          duration={500}
+          className="decktopMenuListItem"
+        >
           Home
         </Link>
-        <Link to="/about" className="decktopMenuListItem">
+        <Link
+          activeClass="active"
+          to="skills"
+          spy={true}
+          smooth={true}
+          offset={-50}
+          duration={500}
+          className="decktopMenuListItem"
+        >
           About
         </Link>
-        <Link to="/portfolio" className="decktopMenuListItem">
+        <Link
+          activeClass="active"
+          to="works"
+          spy={true}
+          smooth={true}
+          offset={-50}
+          duration={500}
+          className="decktopMenuListItem"
+        >
           Portfolio
         </Link>
-        <Link to="/clients" className="decktopMenuListItem">
+        <Link
+          activeClass="active"
+          to="clients"
+          spy={true}
+          smooth={true}
+          offset={-50}
+          duration={500}
+          className="decktopMenuListItem"
+        >
           Clients
         </Link>
       </div>
-      <button className="desktopMenuBtn">
-        <img src={contactImg} alt="" className="desktopMenuImg" />
+      <button
+        className="desktopMenuBtn"
+        onClick={() => {
+          document
+            .getElementById("contact")
+            .scrollIntoView({ behavior: "smooth" });
+        }}
+      >
+        <img src={contactImg} alt="Contact" className="decktopMenuListItem" />
         Contact Me
       </button>
+      <img
+        src={menu}
+        alt="Menu"
+        className="mobMenu"
+        onClick={() => setShowMenu(!showMenu)}
+      />
+      <div className="navMenu" style={{ display: showMenu ? "flex" : "none" }}>
+        <Link
+          activeClass="active"
+          to="intro"
+          spy={true}
+          smooth={true}
+          offset={-100}
+          duration={500}
+          className="listItem"
+          onClick={() => setShowMenu(false)}
+        >
+          Home
+        </Link>
+        <Link
+          activeClass="active"
+          to="skills"
+          spy={true}
+          smooth={true}
+          offset={-50}
+          duration={500}
+          className="listItem"
+          onClick={() => setShowMenu(false)}
+        >
+          About
+        </Link>
+        <Link
+          activeClass="active"
+          to="works"
+          spy={true}
+          smooth={true}
+          offset={-50}
+          duration={500}
+          className="listItem"
+          onClick={() => setShowMenu(false)}
+        >
+          Portfolio
+        </Link>
+        <Link
+          activeClass="active"
+          to="clients"
+          spy={true}
+          smooth={true}
+          offset={-50}
+          duration={500}
+          className="listItem"
+          onClick={() => setShowMenu(false)}
+        >
+          Clients
+        </Link>
+        <Link
+          activeClass="active"
+          to="contact"
+          spy={true}
+          smooth={true}
+          offset={-50}
+          duration={500}
+          className="listItem"
+          onClick={() => setShowMenu(false)}
+        >
+          Contact
+        </Link>
+      </div>
     </nav>
   );
 };
