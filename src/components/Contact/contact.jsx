693318a1d210341daa88597974a706dd7e31da1d
nithin7316 <nithinkumar6330.nk@gmail.com>
2024-06-20 01:33:20 +0530

    links update

--- src/components/Contact/contact.jsx
+++ src/components/Contact/contact.jsx
@@ -4,9 +4,9 @@ import Walmart from "../../assets/walmart.png";
 import Adobe from "../../assets/adobe.png";
 import Microsoft from "../../assets/microsoft.png";
 import Facebook from "../../assets/facebook.png";
-import FacebookIcon from "../../assets/facebook-icon.png";
-import TwitterIcon from "../../assets/twitter.png";
-import YoutubeIcon from "../../assets/youtube.png";
+import Twitter from "../../assets/twitter.png";
+import Github from "../../assets/github.png";
+import Linkedin from "../../assets/linkedin.png";
 import InstagramIcon from "../../assets/instagram.png";
 import emailjs from "@emailjs/browser";
 
@@ -77,10 +77,34 @@ const Contact = () => {
             Submit
           </button>
           <div className="links">
-            <img src={FacebookIcon} alt="Facebook" className="link" />
-            <img src={TwitterIcon} alt="Twitter" className="link" />
-            <img src={YoutubeIcon} alt="YouTube" className="link" />
-            <img src={InstagramIcon} alt="Instagram" className="link" />
+            <a
+              href="https://github.com/Nithin7406"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src={Github} alt="Github" className="link" />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/nithinkumara/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src={Linkedin} alt="Linkedin" className="link" />
+            </a>
+            <a
+              href="https://x.com/Nithin740615258"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src={Twitter} alt="Twitter" className="link" />
+            </a>
+            <a
+              href="https://www.instagram.com/n_i_t_h_i_n_7316"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src={InstagramIcon} alt="Instagram" className="link" />
+            </a>
           </div>
         </form>
       </div>
